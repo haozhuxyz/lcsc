9800f5d5f364e35f442d5c67ffd63a6393f43cf9
ba-ba-ba <15833097098@163.com>
2019-07-15 17:27:53 +0800

    首页路径改为'/'

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,32 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './assets/css/reset.css'
-import {
-  BrowserRouter as Router,
-  NavLink,
-  Route
-} from "react-router-dom"
 
-import Home from "./views/Home"
-import Classify from "./views/Classify"
-import Cart from "./views/Cart"
-import My from "./views/My"
+import FooterNav from "./components/FooterNav"
+
 
 class App extends React.Component{
   render(){
     return (
       <div className="App">
-        <Router>
-          <NavLink to={"/Home"} exact >首页</NavLink>|
-          <NavLink to={"/Classify"}>分类</NavLink>|
-          <NavLink to={"/Cart"}>购物车</NavLink>|
-          <NavLink to={"/My"}>个人中心</NavLink>
-  
-          <Route path={"/Home"} exact component={Home}></Route>
-          <Route path={"/Classify"} component={Classify}></Route>
-          <Route path={"/Cart"} component={Cart}></Route>
-          <Route path={"/My"} component={My}></Route>
-        </Router>
+        <FooterNav></FooterNav>
       </div>
     );
   }
