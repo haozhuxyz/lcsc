172bf34db952a02abf7463dd7402d227b9261cc9
zhuhao <zhuhao4130@163.com>
2019-07-17 22:23:49 +0800

    购物车样式

--- my-app/src/views/Cart.js
+++ my-app/src/views/Cart.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "../assets/css/Cart.css";
 import la from "../assets/img/my1.png";
+import shop0 from "../assets/img/shop0.jpg"
 
 class Cart extends React.Component{
     constructor(){
@@ -45,7 +46,50 @@ class Cart extends React.Component{
 
                 </div>
                 <div className={"cartmain"} style={{display:this.state.isLogin?"block":"none"}}>
-                    cartmain
+                    <div className={"cart_h"}>
+                        <div>
+                            <input type="checkbox" className={"check_all"}/>
+                        </div>
+                        <h3>商品信息</h3>
+                        <span>操作</span>
+
+                    </div>
+
+                    <div className={"myshop"}>
+
+                        <div className={"product_list"} data-pid={"275"}>
+                            <div className={"input_check"}>
+                                <input type="checkbox" name={"pids"} value={"275"} className={"pids"}/>
+                            </div>
+                            <div className={"cart_goods_box"}>
+                                <img src={shop0} alt=""/>
+                                <p>
+                                    <a className={"product_name"}>诛仙3-树妖抱枕</a>
+                                    <span className={"price"}>￥49.00</span>
+
+                                </p>
+
+                            </div>
+                            <div className={"buy_num_wrap"}>
+                                <span className={"num_minus"}>-</span>
+                                <span className={"buy_num"}>1</span>
+                                <span className={"num_add"}>+</span>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                    <div className={"mysum"}>
+                        <div className={"sumleft"}>
+                            <p className={"money"}>￥0.00</p>
+                            <div><span>商品</span><b>0</b></div>
+                        </div>
+
+                        <a className={"tj"}>提交订单</a>
+
+                    </div>
+
                 </div>
 
 
