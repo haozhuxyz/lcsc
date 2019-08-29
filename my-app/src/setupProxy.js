c6df7b31ad009a17b550217b58a2ac29a0f91f08
ba-ba-ba <50904931+ba-ba-ba@users.noreply.github.com>
2019-08-29 18:12:55 +0800

    Update setupProxy.js
    测试

--- my-app/src/setupProxy.js
+++ my-app/src/setupProxy.js
@@ -2,6 +2,7 @@ const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
   app.use(
+  // proxy代理    
     proxy("/proxy", {
       target: "https://www.linkpet.com.cn/Mobile/",
       changeOrigin: true,
